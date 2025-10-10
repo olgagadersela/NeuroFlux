725e743b0c84912766675158db48395ff4c8bd9b
olgagadersela <151327016+olgagadersela@users.noreply.github.com>
2025-10-10 23:27:59 +0500

    Update main.py

--- main.py
+++ main.py
@@ -43,8 +43,8 @@ TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 
-if not TELEGRAM_TOKEN:
-    raise ValueError("❌ TELEGRAM_TOKEN не найден. Проверь .env (локально) или Railway Variables.")
+#if not TELEGRAM_TOKEN:
+#    raise ValueError("❌ TELEGRAM_TOKEN не найден. Проверь .env (локально) или Railway Variables.")
 if not OPENAI_API_KEY:
     raise ValueError("OPENAI_API_KEY не задан в .env")
 
